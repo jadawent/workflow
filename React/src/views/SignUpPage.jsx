2b2588c1e96cf1a96e2a80d5e2f37a3fb6cdaeb9
Tenzin1799 <131330402+Tenzin1799@users.noreply.github.com>
2025-02-20 18:47:51 -0600

    Finished sign up feature. However users are not kept signed in yet in their session.

--- React/src/views/SignUpPage.jsx
+++ React/src/views/SignUpPage.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styles from "../SignUpPage.module.css"
 
 function SignUpPage(){
+    const navigate = useNavigate();
     return (
         <>
             <header className={styles.header}>
@@ -10,30 +11,67 @@ function SignUpPage(){
             <main className={styles.main}>
                 <div className={styles.signUpDiv}>
                     <p className={styles.caption}>For management only!</p>
-                    <p className={styles.username}>Username</p>
+                    <p>First Name*</p>
+                    <input required id="firstName"></input>
+                    <p>Last Name*</p>
+                    <input id="lastName"></input>
+                    <p>Username*</p>
                     <input id="username"></input>
-                    <p>Password</p>
+                    <p>Password*</p>
                     <input type="password" id="password"></input>
-                    <button className={styles.btn} onClick={createUser}>Sign Up</button>
-                    <p className={styles.caption} id="result"></p>
+                    <p>Confirm Password*</p>
+                    <input type="password" id="confirmPassword"></input>
+                    <button className={styles.btn} onClick={() => signUp(navigate)}>Sign Up</button>
                 </div>
             </main>
         </>
     );
 }
 
+function doPasswordsMatch(pw, confirmPw){
+    if (pw === confirmPw){
+        return true;
+    }
+    return false;
+}
 
-async function createUser(){
-    const user = document.getElementById("username");
-    const pw = document.getElementById("password");
+
+function signUp(navigate){
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const user = document.getElementById("username").value;
+    const pw = document.getElementById("password").value;
+    const confirmPw = document.getElementById("confirmPassword").value;
     const displayResult = document.getElementById("result");
+    if(allValuesFilled(firstName, lastName, user, pw, confirmPw)){
+        if (doPasswordsMatch(pw, confirmPw) == true){
+            createUser(firstName, lastName, user, pw, confirmPw, displayResult, navigate);
+         } else {
+             alert("Passwords do not match. Try again.");
+         }
+    } else {
+        alert("Fill out each section");
+    }
+}
+
+function allValuesFilled(firstName, lastName, user, pw, confirmPassword){
+    // Theres GOTTA be a simpler way to do this.
+    if(firstName.length == 0 || lastName.length == 0 ||
+        user.length == 0 || pw.length == 0 || confirmPassword.length == 0){
+        return false;
+    }
+    return true;
+}
+
+async function createUser(firstName, lastName, user, pw, confirmPw, displayResult, navigate){
     const url = "http://localhost:8080/api/create-user"; 
     // can change to https for secure transfer, but we will need to implement TLS/SSL in springboot too.
     const data = {
-        username: user.value,
-        password: pw.value,
-        firstName: "Lady",
-        lastName: "Gaga"
+        username: user,
+        password: pw,
+        confirmPassword: confirmPw,
+        firstName: firstName,
+        lastName: lastName
     }
     try{
         const response = await fetch(url , {
@@ -45,17 +83,18 @@ async function createUser(){
         });
         const result = await response.text();
         
-        if (!response.ok){
+        if(response.ok){
             console.log(result);
-            displayResult.innerText = "Sign up failed.";
-            throw new Error(result);
+            alert("Sign up success!");
+            navigate("/")
         }
-        console.log("User created:", result);
-        displayResult.innerText = "Sign up success!";
+        else {
+            console.log(result);
+            alert(result);
+        }
     } catch (error) {
         console.error(error);
     }
 }
 
 export default SignUpPage;
-  
--- Springboot/src/main/java/com/workflow/controllers/Controller.java
+++ Springboot/src/main/java/com/workflow/controllers/Controller.java
@@ -1,10 +1,14 @@
 package com.workflow.controllers;
 
+import com.workflow.models.Role;
+import com.workflow.services.RoleService;
 import com.workflow.services.UserService;
 import com.workflow.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -13,11 +17,29 @@ public class Controller {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
     @PostMapping("/create-user")
     private ResponseEntity<?> createUser(@RequestBody User user){
         try{
-            User createdUser = userService.createUser(user);
-            return ResponseEntity.ok(createdUser);
+            // Trims trailing spaces and
+            // checks if username already exists
+            user.setUsername(user.getUsername().trim());
+            if (userService.existsByUsername(user.getUsername())) {
+                return ResponseEntity.badRequest().body("Username is already taken.");
+            }
+
+            // checks if the 2 passwords match before creating the user.
+            if(!user.getPassword().equals(user.getConfirmPassword())){
+                return ResponseEntity.badRequest().body("Passwords do not match.");
+            }
+
+            // Creates a manager role if not already existing in database
+            Role role = roleService.saveAsManagerRole();
+
+            userService.createUser(user, List.of(role));
+            return ResponseEntity.ok("User created successfully.");
         } catch (Exception e){
             System.out.println(e);
             return ResponseEntity.badRequest().body("Could not create user.");
